c66d8fc5faefd6b293cadee9777b760ec20bf3d2
vladislav149 <vladislav149@mail.ru>
2022-02-23 19:14:05 +0300

    added the ability to remove and add

--- js/main.js
+++ js/main.js
@@ -1,17 +1,75 @@
 import {
-  buttonAddTask
+  buttonAddTask,
+  buttonDeleteTask,
+  textTask
 } from './view.js';
+
+const idInputArr = ['checkbox-0', 'checkbox-1', 'checkbox-2', 'checkbox-3'];
 
 buttonAddTask.forEach(function (item) {
   item.addEventListener("click", addNewTask)
 });
 
+test();
+
+function test() {
+  buttonDeleteTask.forEach(function (item) {
+    item.addEventListener("click", deleteTask)
+  });
+
+  textTask.forEach(function (item) {
+    item.addEventListener("click", changeStatusTask)
+  });
+}
+
+
 function addNewTask() {
-  let ul = document.querySelector('.list-tasks');
-  let input = document.querySelector('.todo__new-task');
+  let parentButton = this.parentElement;
+  let listTask = parentButton.nextElementSibling;
+  let input = parentButton.firstElementChild;
   let textTask = input.value;
-  ul.insertAdjacentHTML('beforeend',
-    `<li class = "list-tasks__item task"><input class="list-tasks__check" type = "checkbox" id="checkbox-4"><label class="list-tasks__text" for="checkbox-4">${textTask}</label><button class="list-tasks__delete btn" type = "button"></button>`
-  );
-  input.value = '';
+  let fredomId = findEmptyIdInput();
+  let isValidId = fredomId < idInputArr.length ? true : false;
+  if (textTask) {
+    listTask.insertAdjacentHTML('beforeend',
+      `<li class="list-tasks__item task">
+        <input class="list-tasks__check" type="checkbox" id="checkbox-${fredomId}">
+        <label class="list-tasks__text" for="checkbox-${fredomId}">${textTask}</label>
+        <button class="list-tasks__delete btn" type="button"></button>
+      </li>`
+    );
+    input.value = '';
+    if (isValidId) {
+      idInputArr.splice(fredomId, 1, `checkbox-${fredomId}`);
+    } else {
+      idInputArr.push(`checkbox-${fredomId}`);
+    }
+  } else {
+    return
+  }
+  test();
+}
+
+function findEmptyIdInput() {
+  let idInput = idInputArr.findIndex(item => item === undefined)
+  if ((Boolean(idInput + 1))) {
+    return idInput;
+  } else {
+    idInput = idInputArr.length;
+  }
+  return idInput;
+}
+
+function deleteTask() {
+  let liDeleteTask = this.parentElement;
+  let input = liDeleteTask.querySelector('input');
+  let idInput = input.id;
+  let idInputIndex = idInputArr.findIndex(item => item === idInput);
+  delete idInputArr[idInputIndex];
+  liDeleteTask.remove();
+}
+
+function changeStatusTask() {
+  let parentTextTask = this.parentElement;
+  parentTextTask.classList.toggle('list-tasks__check--active');
 }
--- js/view.js
+++ js/view.js
@@ -1,1 +1,3 @@
 export const buttonAddTask = document.querySelectorAll('.todo__add-task');
+export const buttonDeleteTask = document.querySelectorAll('.list-tasks__delete');
+export const textTask = document.querySelectorAll('.list-tasks__text');
