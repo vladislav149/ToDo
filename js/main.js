6671b1a8b7aa0d87227d6b67d19aa4441e18e13e
vladislav149 <vladislav149@mail.ru>
2022-02-26 20:43:14 +0300

    splite into view.js and main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-import {
-  buttonAddTask,
-  buttonDeleteTask,
-  textTask
-} from './view.js';
-
 const STATUS = {
   DONE: "Done",
   TO_DO: "To do"
@@ -18,82 +12,7 @@ const DEFAULT_STATUS = STATUS.TO_DO;
 
 const list = [];
 
-buttonAddTask.forEach(function (item) {
-  item.addEventListener("click", addNewTask)
-});
-
-buttonDeleteTask.forEach(function (item) {
-  item.addEventListener("click", deleteTask)
-});
-
-textTask.forEach(function (item) {
-  item.addEventListener("click", changeStatusTask)
-});
-
-function addNewTask() {
-  let parentButton = this.parentElement;
-  let listTask = parentButton.nextElementSibling;
-  let input = parentButton.firstElementChild;
-  let fredomId = findEmptyIdInput();
-  let textTask = input.value.trim();
-  let priority = checkPriority(parentButton);
-
-  if (textTask) {
-    listTask.insertAdjacentHTML('beforeend',
-      `<li class="list-tasks__item task">
-        <input class="list-tasks__check" type="checkbox" id="checkbox-${fredomId}">
-        <label class="list-tasks__text" for="checkbox-${fredomId}">${textTask}</label>
-        <button class="list-tasks__delete btn" type="button"></button>
-      </li>`
-    );
-    input.value = '';
-    let id = `checkbox-${fredomId}`;
-    addTask(fredomId, textTask, priority, id);
-  } else {
-    input.value = '';
-    return
-  }
-
-  let liNewTask = listTask.lastChild.querySelector('.list-tasks__text');
-  liNewTask.addEventListener("click", changeStatusTask);
-
-  let buttonDelete = listTask.lastChild.querySelector('.list-tasks__delete');
-  buttonDelete.addEventListener("click", deleteTask);
-}
-
-function findEmptyIdInput() {
-  let idInput = list.findIndex(item => item === undefined)
-  let isValiIdInput = (Boolean(idInput + 1)) ? true : false;
-  if (isValiIdInput) {
-    return idInput;
-  } else {
-    idInput = list.length;
-  }
-  return idInput;
-}
-
-function deleteTask() {
-  let liDeleteTask = this.parentElement;
-  let input = liDeleteTask.querySelector('input');
-  let idInput = input.id;
-  let idInputIndex = list.findIndex(item => {
-    if (!item) {
-      console.log('error');
-    } else if (item.id === idInput) {
-      return item.id
-    }
-  });
-  liDeleteTask.remove();
-  delete list[idInputIndex];
-}
-
-function changeStatusTask() {
-  changeStatus(this);
-  let parentTextTask = this.parentElement;
-  parentTextTask.classList.toggle('list-tasks__check--active');
-}
-
-function addTask(id, textTask, priority, idInput) {
+export function addTask(id, textTask, priority, idInput) {
   list[id] = {
     name: textTask,
     status: DEFAULT_STATUS,
@@ -102,20 +21,41 @@ function addTask(id, textTask, priority, idInput) {
   };
 }
 
-function checkPriority(parentButton) {
-  if (parentButton.classList.contains('high-priority')) {
+export function findEmptyIdInput() {
+  let idInput = list.findIndex(item => item === undefined);
+  let isValiIdInput = Boolean(idInput + 1);
+  if (isValiIdInput) {
+    return idInput;
+  } else {
+    return idInput = list.length;
+  }
+}
+
+export function changeStatus(idTask) {
+  let whichId = list.findIndex(item => item.id === idTask);
+  if (list[whichId].status === STATUS.TO_DO) {
+    list[whichId].status = STATUS.DONE
+  } else {
+    list[whichId].status = STATUS.TO_DO
+  }
+}
+
+export function checkPriority(checkClass) {
+  if (checkClass) {
     return PRIORITY.HIGH;
   } else {
     return PRIORITY.LOW;
   }
 }
 
-function changeStatus(label) {
-  let labelfor = label.htmlFor;
-  let whichId = list.findIndex(item => item.id === labelfor);
-  if (list[whichId].status === STATUS.TO_DO) {
-    list[whichId].status = STATUS.DONE
-  } else {
-    list[whichId].status = STATUS.TO_DO
-  }
+export function deleteTaskInList(input) {
+  let idInput = input.id;
+  let idInputIndex = list.findIndex(item => {
+    if (!item) {
+      console.log('error');
+    } else if (item.id === idInput) {
+      return item.id
+    }
+  });
+  delete list[idInputIndex];
 }
--- js/view.js
+++ js/view.js
@@ -1,3 +1,60 @@
-export const buttonAddTask = document.querySelectorAll('.todo__add-task');
-export const buttonDeleteTask = document.querySelectorAll('.list-tasks__delete');
-export const textTask = document.querySelectorAll('.list-tasks__text');
+import {
+  addTask,
+  findEmptyIdInput,
+  changeStatus,
+  deleteTaskInList,
+  checkPriority
+} from './main.js';
+
+
+const buttonAddTask = document.querySelectorAll('.todo__add-task');
+
+buttonAddTask.forEach(function (item) {
+  item.addEventListener("click", addNewTask)
+});
+
+function addNewTask() {
+  let parentButton = this.parentElement;
+  let listTask = parentButton.nextElementSibling;
+  let input = parentButton.firstElementChild;
+  let fredomId = findEmptyIdInput();
+  let textTask = input.value.trim();
+  let checkClass = parentButton.classList.contains('high-priority');
+  let priority = checkPriority(checkClass);
+
+  if (textTask) {
+    listTask.insertAdjacentHTML('beforeend',
+      `<li class="list-tasks__item task">
+        <input class="list-tasks__check" type="checkbox" id="checkbox-${fredomId}">
+        <label class="list-tasks__text" for="checkbox-${fredomId}">${textTask}</label>
+        <button class="list-tasks__delete btn" type="button"></button>
+      </li>`
+    );
+    input.value = '';
+    let id = `checkbox-${fredomId}`;
+    addTask(fredomId, textTask, priority, id);
+  } else {
+    input.value = '';
+    return
+  }
+
+  let liNewTask = listTask.lastChild.querySelector('.list-tasks__text');
+  liNewTask.addEventListener("click", changeStatusTask);
+
+  let buttonDelete = listTask.lastChild.querySelector('.list-tasks__delete');
+  buttonDelete.addEventListener("click", deleteTask);
+}
+
+function changeStatusTask() {
+  let parentTextTask = this.parentElement;
+  let labelFor = this.htmlFor;
+  changeStatus(labelFor);
+  parentTextTask.classList.toggle('list-tasks__check--active');
+}
+
+function deleteTask() {
+  let liDeleteTask = this.parentElement;
+  let input = liDeleteTask.querySelector('input');
+  deleteTaskInList(input);
+  liDeleteTask.remove();
+}
